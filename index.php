b910a21c0bfaa3ab6c7bbda245be12867ae6544e
imran <imranshaikh@gmail.com>
2014-02-27 16:33:33 +0530

    Item dropdown added

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
   $name = $_POST['name'];
   $contact = $_POST['contact'];
-  $human = $_POST['human'];
+  $items = strtoupper($_POST['items']);
   $from = $_POST['email'];
-  $to = 'kafil.ahmedtd@gmail.com, imranshaikh818@gmail.com';
+  $to = 'kafil.ahmedtd@gmail.com, shahbaazmacchiwala@gmial.com';
   $subject = 'New order request' ;
   $body = "Hi $name \n";
   $body .=  "My contact no: $contact \n";
-  $body .= "My Order is :" ;
+  $body .= "My Order is : $items" ;
   $error_msg = "";
   $error = "";
-  if($_POST['submit'] && $human == '4'){
+  if($_POST['submit']){
     if(mail($to, $subject, $body, $from)){
       $error = 1;
     }else{
@@ -28,15 +28,18 @@
     <meta name="keywords" content="" />
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" rel="stylesheet" type="text/css" />
     <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
-    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-2.0.3.min.js"></script>
     <script src="js/jquery.dropotron.min.js"></script>
     <script src="js/skel.min.js"></script>
     <script src="js/skel-panels.min.js"></script>
     <script src="js/init.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    
+    <script type="text/javascript" src="js/bootstrap-3.0.3.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap-multiselect.js"></script>
+    
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-3.0.3.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-multiselect.css">
+
     <noscript>
       <link rel="stylesheet" href="css/skel-noscript.css" />
       <link rel="stylesheet" href="css/style.css" />
@@ -111,10 +114,36 @@
                     </td>
                   </tr>
                   <tr>
-                    <td>*What is 2+2? (Anti-spam)</td>
+                    <td>Pick your choice</td>
+                    <td>
+                      
+                      <select id="items" name="items" multiple="multiple">
+                        <option value="pomfret">Pomfret</option>
+                        <option value="kafree">Kafree</option>
+                        <option value="prawns">Prawns</option>
+                        <option value="Rawas">Rawas</option>
+                        <option value="surmai">Surmai</option>
+                        <option value="halwa">Halwa</option>
+                        <option value="dara">Dara</option>
+                        <option value="ghol">Ghol</option>
+                        <option value="bombil">Bombil</option>
+                        <option value="bangda">Bangda</option>
+                        <option value="bass">Bass (Khajura)</option>
+                        <option value="karkara">Karkara</option>
+                        <option value="basa">Basa</option>
+                        <option value="whiteprawns">Whiteprawns</option>
+                        <option value="crabs">Crabs</option>
+                        <option value="vakru">Vakru</option>
+                        <option value="lobster">Lobster</option>
+                      </select>
+                    
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Enquiry</td>
                     <td>
                       <div class="input-append">
-                        <input type="text" name="human" class="form-control" placeholder="Type here" required />
+                        <textarea value="enquiry"></textarea>  
                       </div>
                     </td>
                   </tr>
@@ -229,137 +258,12 @@
     <!-- Footer -->
       <div id="footer">
         <div class="container">
-          <!--  <div class="row">-->
-            
-            <!-- Tweets -->
-            <!--
-              <section class="4u">
-                <header>
-                  <h2 class="fa fa-twitter circled solo"><span>Tweets</span></h2>
-                </header>
-                <ul class="divided">
-                  <li>
-                    <article class="tweet">
-                      Amet nullam fringilla nibh nulla convallis tique ante sociis accumsan.
-                      <span class="timestamp">5 minutes ago</span>
-                    </article>
-                  </li>
-                  <li>
-                    <article class="tweet">
-                      Hendrerit rutrum quisque.
-                      <span class="timestamp">30 minutes ago</span>
-                    </article>
-                  </li>
-                  <li>
-                    <article class="tweet">
-                      Curabitur donec nulla massa laoreet nibh. Lorem praesent montes.
-                      <span class="timestamp">3 hours ago</span>
-                    </article>
-                  </li>
-                  <li>
-                    <article class="tweet">
-                      Lacus natoque cras rhoncus curae dignissim ultricies. Convallis orci aliquet.
-                      <span class="timestamp">5 hours ago</span>
-                    </article>
-                  </li>
-                </ul>
-              </section>
-              -->
-            <!-- Posts -->
-             <!--
-              <section class="4u">
-                <header>
-                  <h2 class="fa fa-file circled solo"><span>Posts</span></h2>
-                </header>
-                <ul class="divided">
-                  <li>
-                    <article class="post stub">
-                      <header>
-                        <h3><a href="#">Nisl fermentum integer</a></h3>
-                      </header>
-                      <span class="timestamp">3 hours ago</span>
-                    </article>
-                  </li>
-                  <li>
-                    <article class="post stub">
-                      <header>
-                        <h3><a href="#">Phasellus portitor lorem</a></h3>
-                      </header>
-                      <span class="timestamp">6 hours ago</span>
-                    </article>
-                  </li>
-                  <li>
-                    <article class="post stub">
-                      <header>
-                        <h3><a href="#">Magna tempus consequat</a></h3>
-                      </header>
-                      <span class="timestamp">Yesterday</span>
-                    </article>
-                  </li>
-                  <li>
-                    <article class="post stub">
-                      <header>
-                        <h3><a href="#">Feugiat lorem ipsum</a></h3>
-                      </header>
-                      <span class="timestamp">2 days ago</span>
-                    </article>
-                  </li>
-                </ul>
-              </section>
-            -->  
-            <!-- Photos -->
-            <!--
-              <section class="4u">
-                <header>
-                  <h2 class="fa fa-camera circled solo"><span>Photos</span></h2>
-                </header>
-                <div class="row quarter no-collapse">
-                  <div class="6u">
-                    <a href="http://mdomaradzki.deviantart.com/art/Air-Lounge-385212062" class="image full"><img src="images/pic10.jpg" alt="" /></a>
-                  </div>
-                  <div class="6u">
-                    <a href="http://mdomaradzki.deviantart.com/art/Paris-City-Streets-II-382623606" class="image full"><img src="images/pic11.jpg" alt="" /></a>
-                  </div>
-                </div>
-                <div class="row quarter no-collapse">
-                  <div class="6u">
-                    <a href="http://mdomaradzki.deviantart.com/art/Trainride-Visions-383309985" class="image full"><img src="images/pic12.jpg" alt="" /></a>
-                  </div>
-                  <div class="6u">
-                    <a href="http://mdomaradzki.deviantart.com/art/Paris-Metro-382623851" class="image full"><img src="images/pic13.jpg" alt="" /></a>
-                  </div>
-                </div>
-                <div class="row quarter no-collapse">
-                  <div class="6u">
-                    <a href="http://mdomaradzki.deviantart.com/art/Cliffs-of-Coogee-II-366961860" class="image full"><img src="images/pic14.jpg" alt="" /></a>
-                  </div>
-                  <div class="6u">
-                    <a href="http://mdomaradzki.deviantart.com/art/Stormy-Coast-VII-366561367" class="image full"><img src="images/pic15.jpg" alt="" /></a>
-                  </div>
-                </div>
-              </section>
-
-          </div>
-          <hr />
-          -->
           <div class="row">
             <div class="12u">
-              
               <!-- Contact -->
-                <section class="contact">
-                <!--  <header>
-                    <h3>Fresh fishes available</h3>
-                    </header> -->
-                  <p>Free home delivery and order need to be placed before 24 hours. </p>
-                  <!-- <ul class="icons">
-                    <li><a href="#" class="fa fa-twitter solo"><span>Twitter</span></a></li>
-                    <li><a href="#" class="fa fa-facebook solo"><span>Facebook</span></a></li>
-                    <li><a href="#" class="fa fa-google-plus solo"><span>Google+</span></a></li>
-                    <li><a href="#" class="fa fa-pinterest solo"><span>Pinterest</span></a></li>
-                    <li><a href="#" class="fa fa-dribbble solo"><span>Dribbble</span></a></li>
-                    <li><a href="#" class="fa fa-linkedin solo"><span>Linkedin</span></a></li>
-                  </ul> -->
-                </section>
+              <section class="contact">
+                <p>Free home delivery and order need to be placed before 24 hours. </p>
+              </section>
               
               <!-- Copyright -->
                 <div class="copyright">
@@ -376,3 +280,13 @@
 
   </body>
 </html>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('#items').multiselect({
+      enableCaseInsensitiveFiltering: true, 
+      includeSelectAllOption: true,
+      enableFiltering: true,
+      maxHeight: 240
+    });
+  });
+</script>
